cc1b040ef8849fbd4fcbd7026712be3994b38631
Naker90 <Antoniojesussg96@gmail.com>
2020-04-04 22:09:14 +0200

    implemented ChargeTrafficTicketService

--- Examples/Option/Domain/TrafficTickets/ChargeTrafficTicketService.cs
+++ Examples/Option/Domain/TrafficTickets/ChargeTrafficTicketService.cs
@@ -6,26 +6,71 @@ namespace Examples.Option.Domain.TrafficTickets
     public sealed class ChargeTrafficTicketService
     {
         private readonly DriverRepository driverRepository;
+        private readonly TrafficTicketRepository trafficTicketRepository;
 
-        public ChargeTrafficTicketService(DriverRepository driverRepository)
+        public ChargeTrafficTicketService(
+            DriverRepository driverRepository,
+            TrafficTicketRepository trafficTicketRepository)
         {
             this.driverRepository = driverRepository;
+            this.trafficTicketRepository = trafficTicketRepository;
         }
 
-        Either<Error, TrafficTicket> Execute() 
+        Either<Error, TrafficTicket> Execute(TrafficTicketChargeRequest request)
         {
-            return null;
+            return
+                from driver in SearchDriverBy(request.DriverPersonalIdentificationCode)
+                from trafficTicket in SearchTrafficTicketBy(request.TrafficTicketId)
+                from chargeId in PayTrafficTicket(
+                    driver: driver, 
+                    trafficTicket: trafficTicket)
+                from _ in MarkTrafficTicketAsPaid(
+                    trafficTicket: trafficTicket, 
+                    chargeId: chargeId)
+                select trafficTicket;
+        }
+
+        Either<Error, Driver.Driver> SearchDriverBy(string personalIdentificationCode) 
+        {
+            return driverRepository
+                .SearchBy(personalIdentificationCode: personalIdentificationCode)
+                .ToEither(() => Error.DriverNotFound);
+        }
+
+        Either<Error, TrafficTicket> SearchTrafficTicketBy(string trafficTicketId)
+        {
+            return trafficTicketRepository
+                .SearchBy(id: trafficTicketId)
+                .ToEither(() => Error.TrafficTicketNotFound);
+        }
+
+        Either<Error, string> PayTrafficTicket(
+            TrafficTicket trafficTicket,
+            Driver.Driver driver)
+        {
+            //pay traffic ticket with stripe for example using driver object 
+            //for extract driver information.
+            return "chargeId";
+        }
+
+        Either<Error, Unit> MarkTrafficTicketAsPaid(
+            TrafficTicket trafficTicket,
+            string chargeId)
+        {
+            trafficTicket.MarkAsPaid(chargeId);
+            return Prelude.unit;
         }
     }
 
     public sealed class TrafficTicketChargeRequest 
     {
         public string TrafficTicketId { get; }
-        public string UserPersonalIdentificationCode { get; }
+        public string DriverPersonalIdentificationCode { get; }
     }
 
     public enum Error 
     {
-        UserNotFound 
+        DriverNotFound,
+        TrafficTicketNotFound
     }
 }
--- Examples/Option/Domain/TrafficTickets/TrafficTicket.cs
+++ Examples/Option/Domain/TrafficTickets/TrafficTicket.cs
@@ -1,9 +1,20 @@
-﻿namespace Examples.Option.Domain.TrafficTickets
+﻿using System;
+using LanguageExt;
+
+namespace Examples.Option.Domain.TrafficTickets
 {
     public sealed class TrafficTicket
     {
         public string Id { get; }
         public double Ammount { get; }
         public string DriverPersonalIndentificationCode { get; }
+        public bool IsPaid { get; private set; }
+        public Option<string> ChargeId { get; private set; }
+
+        public void MarkAsPaid(string chargeId)
+        {
+            IsPaid = true;
+            ChargeId = chargeId;
+        }
     }
 }
