32b64ffd7db9f9d897ecd95b16f29135391be36f
Naker90 <Antoniojesussg96@gmail.com>
2020-04-11 22:33:33 +0200

    created application service

---
+++ Tests/Application/SearchDriverFromTrafficTicketServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace Tests.Application
+{
+    [TestFixture]
+    public sealed class SearchDriverFromTrafficTicketServiceTests
+    {
+        private Mock<Examples.Application.TrafficTicketRepository> trafficTicketRepository;
+        private Mock<Examples.Application.DriverRepository> driverRepository;
+        private Examples.Application.SearchDriverFromTrafficTicketService service;
+
+        [SetUp]
+        public void SetUp() 
+        {
+            trafficTicketRepository = new Mock<Examples.Application.TrafficTicketRepository>();
+            driverRepository = new Mock<Examples.Application.DriverRepository>();
+            service = new Examples.Application.SearchDriverFromTrafficTicketService(
+                trafficTicket: trafficTicketRepository.Object,
+                driverRepository: driverRepository.Object);
+        }
+
+        [Test]
+        public void GetsDriveFullName() 
+        {
+            int trafficTicketId = 1;
+            var trafficTicket = new Examples.Application.TrafficTicket(
+                driverPersonalIdentificationCode: "422345456P");
+            trafficTicketRepository
+                .Setup(x => x.UnsafeSearchBy(trafficTicketId))
+                .Returns(trafficTicket);
+            var driver = new Examples.Application.Driver(
+                firstName: "Alvaro",
+                lastName: "Gonzalez");
+            driverRepository
+                .Setup(x => x.UnsafeSearchBy(trafficTicket.DriverPersonalIdentificationCode))
+                .Returns(driver);
+
+            var driverFullName = service.UnsafeGetFullName(trafficTicketId);
+
+            driverFullName.Should().Be($"{driver.FirstName} {driver.LastName}");
+        }
+    }
+}
