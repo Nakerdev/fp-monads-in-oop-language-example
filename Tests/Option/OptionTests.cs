4eb5586470b3d4a42b501947a0d8f8add88375ae
Naker90 <Antoniojesussg96@gmail.com>
2020-04-11 18:53:54 +0200

    made tests to explain optional types behavior

--- Tests/Option/OptionTests.cs
+++ Tests/Option/OptionTests.cs
@@ -8,6 +8,19 @@ namespace Tests.Option
     public class OptionTests
     {
         [Test]
+        public void NullableType() 
+        {
+            int? nullableInt = null;
+            //You can't mark all types as nullable.
+            //string? nullableInt?? = null;
+
+            //You can access to nullable value. It fails in runtime, not in compile time.
+            //nullableInt.Value.Should().Be(1);
+
+            nullableInt.HasValue.Should().BeFalse();
+        }
+
+        [Test]
         public void OptionalType()
         {
             Option<string> stringWithValue = "foo";
@@ -37,6 +50,46 @@ namespace Tests.Option
             //NullReferenceException
             nullableInt.Value.Should().Be(1);
             optionalInt.IfSome(number => number.Should().Be(1));
+        }
+
+        [Test]
+        public void OptionalTypesPublicMethods() 
+        {
+            Option<string> optionalString = "1";
+
+            optionalString.IsSome.Should().BeTrue();
+            optionalString.IsNone.Should().BeFalse();
+
+            //These methods returns Unit(void) cause you can't reuturn values 
+            //if not handle both branchs.
+            optionalString.IfSome(value => value.Should().Be("1"));
+            optionalString.IfNone(() => true.Should().BeTrue());
+
+            //Match can returns Unit(void) or the inner type of the Option.
+            optionalString.Match(
+                None: () => true.Should().BeTrue(),
+                Some: value => value.Should().Be("1"));
+
+            //You can return another string value but can't map to another type.
+            var optionalValue = optionalString.Match(
+                None: () => string.Empty,
+                Some: value => value);
+            optionalValue.Should().Be("1");
+
+            //When you map the type the result will be another optional type.
+            var number = optionalString.Map(value => int.Parse(value));
+            number.Match(
+                None: () => true.Should().BeTrue(),
+                Some: num => num.Should().Be(1));
+
+            //You can convert optional types to null using unsafe methods.
+
+            var possibleNullValue = optionalString.MatchUnsafe(
+                None: () => null,
+                Some: value => value);
+            possibleNullValue.Should().Be("1");
+
+            var posibleNullValue2 = optionalString.IfNoneUnsafe(() => null);
         }
     }
 }
